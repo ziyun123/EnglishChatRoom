7415d1a9eb7cf6c288891b15e72a20b902aa5b4b
紫云响木 <1392310662@qq.com>
2018-07-03 17:30:19 +0800

    VUE项目

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+    <home-rcommend></home-rcommend>
+    <home-weekend></home-weekend>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/Swiper.vue'
+import HomeIcons from './components/Icons.vue'
+import HomeRcommend from './components/Rcommend.vue'
+import HomeWeekend from './components/Weekend.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRcommend,
+    HomeWeekend
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/fc/e47aa3e1c67bbc02.jpg_750x200_0f3eecf8.jpg'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/a0/f08f045f0b133402.jpg_750x200_007884b9.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #FFF
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.67%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
